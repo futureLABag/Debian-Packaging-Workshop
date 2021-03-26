0c82b0da7afb2ccf504da74d76d4dbcc2c61e864
Manfred Stock <m-gh@nfred.ch>
2021-03-26 23:12:39 +0000

    Add GPG key and reprepro configuration
    The key has to be considered as completely insecure since it is about to
    end up in a public Git repository, it therefore must not be installed on
    a system and be trusted to verify packages or repositories.

---
+++ examples/env.sh
@@ -0,0 +1,5 @@
+# Setup environment to play with examples
+
+export DEBEMAIL=package-signing@example.com
+export DEBFULLNAME="Sample Debian Package and Repository Signing Key (completely insecure)"
+export GNUPGHOME=$PWD/gnupghome
---
+++ examples/prepare.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source env.sh
+
+# Prepare repository
+(
+	cd repository
+	mkdir -pv {incoming,morgue,log,temp}/workshop-testing
+	reprepro -vvv createsymlinks
+)
+
+echo
+echo "Add something like 
+--8<-----------------------------------------------------------------
+package config;
+
+\$cfg{'workshop'} = {
+	method        => 'copy',
+	incoming      => '$PWD/repository/incoming/workshop-testing',
+	dinstall_runs => 1,
+};
+
+1;
+----------------------------------------------------------------->8--
+
+to your ~/.dupload.conf"
+
+# Fetch some source code to package
+(
+	cd packages
+	mkdir -p perl
+	(
+		cd perl
+		wget -c https://cpan.metacpan.org/authors/id/M/MS/MSTOCK/App-Scaffolder-0.002000.tar.gz
+	)
+)
